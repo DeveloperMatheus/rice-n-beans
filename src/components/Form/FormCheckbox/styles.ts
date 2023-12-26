f96234a76a04792620ac6469cc75be0580c4583d
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2023-12-27 00:15:34 +0100

    fix checkbox

--- src/components/Form/FormCheckbox/styles.ts
+++ src/components/Form/FormCheckbox/styles.ts
@@ -1,5 +1,5 @@
 import { cva } from "class-variance-authority";
 
 export const checkboxStyles = cva(
-  "bg-white dark:bg-zinc-950 rounded-md h-5 w-5 cursor-pointer appearance-none border border-opacity-20 dark:border-zinc-800 dark:bg-zinc-950 focus:shadow-none focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 checked:relative checked:before:absolute checked:before:top-2.5 checked:before:left-1/2 checked:before:w-[0.8rem] checked:before:-translate-x-1/2 checked:before:-translate-y-1/2 checked:before:content-checkIcon checked:before:brightness-0 dark:checked:before:brightness-100"
+  "bg-white dark:bg-zinc-950 rounded-md h-5 w-5 cursor-pointer appearance-none border border-zinc-200 dark:border-zinc-800 checked:relative checked:before:absolute checked:before:top-2.5 checked:before:left-1/2 checked:before:w-[0.8rem] checked:before:-translate-x-1/2 checked:before:-translate-y-1/2 checked:before:content-checkIcon checked:before:brightness-0 dark:checked:before:brightness-200"
 );

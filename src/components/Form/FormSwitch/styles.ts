4c71169cfdb5da518b95bee92b8342486e3c4d0e
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-01-08 23:41:48 +0100

    fix opacity when disabled

--- src/components/Form/FormSwitch/styles.ts
+++ src/components/Form/FormSwitch/styles.ts
@@ -1,5 +1,5 @@
 import { cva } from "class-variance-authority";
 
 export const switchStyles = cva(
-  "appearance-none cursor-pointer bg-white dark:bg-zinc-950 w-12 h-6 border border-zinc-200 dark:border-zinc-800 rounded-full relative before:absolute before:top-[0.88rem] before:-translate-x-1 checked:before:translate-x-[1.25rem] before:-translate-y-1/2 before:transition-all before:content-circleIcon before:brightness-[.8] dark:before:brightness-[.4] checked:before:brightness-0 dark:checked:before:brightness-200 disabled:opacity-50 disabled:cursor-not-allowed"
+  "appearance-none cursor-pointer bg-white dark:bg-zinc-950 w-12 h-6 border border-zinc-200 dark:border-zinc-800 rounded-full relative before:absolute before:top-[0.88rem] before:-translate-x-1 checked:before:translate-x-[1.25rem] before:-translate-y-1/2 before:transition-all before:content-circleIcon before:brightness-[.8] dark:before:brightness-[.4] checked:before:brightness-0 dark:checked:before:brightness-200 disabled:opacity-30 disabled:cursor-not-allowed"
 );

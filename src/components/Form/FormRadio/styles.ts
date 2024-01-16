f99060a70303c8d3593a6dc3ad03457f93c8ceaf
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-01-16 21:18:46 +0100

    replace the svg with a formed before pseudo element

--- src/components/Form/FormRadio/styles.ts
+++ src/components/Form/FormRadio/styles.ts
@@ -1,5 +1,5 @@
 import { cva } from "class-variance-authority";
 
 export const radioStyles = cva(
-  "appearance-none cursor-pointer w-5 h-5 bg-white dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-800 rounded-full relative checked:before:absolute checked:before:-translate-y-1/2 checked:before:-translate-x-1/2 checked:before:top-1/2 checked:before:left-1/2  checked:before:content-[''] checked:before:w-[0.6em] checked:before:h-[0.6em] checked:before:rounded-full checked:before:bg-black dark:checked:before:bg-white disabled:cursor-not-allowed disabled:opacity-50"
+  "appearance-none cursor-pointer w-5 h-5 bg-white dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-800 rounded-full relative checked:before:absolute checked:before:-translate-y-1/2 checked:before:-translate-x-1/2 checked:before:top-1/2 checked:before:left-1/2 checked:before:content-[''] checked:before:w-[0.6em] checked:before:h-[0.6em] checked:before:rounded-full checked:before:bg-black dark:checked:before:bg-white disabled:cursor-not-allowed disabled:opacity-50"
 );
--- src/components/Form/FormSwitch/styles.ts
+++ src/components/Form/FormSwitch/styles.ts
@@ -1,5 +1,6 @@
 import { cva } from "class-variance-authority";
 
 export const switchStyles = cva(
-  "appearance-none cursor-pointer w-12 h-6 border border-zinc-200 dark:border-zinc-800 rounded-full relative bg-zinc-200 dark:bg-zinc-950 checked:bg-black checked:dark:bg-white before:absolute before:top-[0.88rem] before:-translate-x-1 checked:before:translate-x-[1.25rem] before:-translate-y-1/2 before:transition-all before:content-circleIcon dark:before:brightness-[.4] checked:before:brightness-200 dark:checked:before:brightness-0 disabled:opacity-30 disabled:cursor-not-allowed"
+  // "appearance-none cursor-pointer w-12 h-6 border border-zinc-200 dark:border-zinc-800 rounded-full relative bg-zinc-200 dark:bg-zinc-950 before:translate-x-2 checked:bg-black checked:dark:bg-white checked:before:absolute checked:before:-translate-y-1/2 checked:before:top-1/2 checked:before:translate-x-8 before:transition-all before:content-[''] before:w-[0.6rem] before:h-[0.6rem] before:rounded-full before:bg-white dark:checked:before:bg-black disabled:opacity-30 disabled:cursor-not-allowed"
+  "appearance-none cursor-pointer w-12 h-6 border border-zinc-200 dark:border-zinc-800 rounded-full relative bg-zinc-200 dark:bg-zinc-950 before:w-[0.7rem] before:h-[0.7rem] before:bg-white before:rounded-full before:absolute before:top-1/2 before:-translate-y-1/2 before:translate-x-1 checked:before:translate-x-[1.92rem] before:transition-all disabled:opacity-30 disabled:cursor-not-allowed"
 );

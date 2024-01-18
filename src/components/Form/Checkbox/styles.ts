6d7a81cebd56e9f82271e3aba3231f330d714b7a
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-01-18 21:21:15 +0100

    apply the correct focus ring

--- src/components/Form/Checkbox/styles.ts
+++ src/components/Form/Checkbox/styles.ts
@@ -1,5 +1,5 @@
 import { cva } from "class-variance-authority";
 
 export const checkboxStyles = cva(
-  "bg-white dark:bg-zinc-950 rounded-md h-5 w-5 cursor-pointer appearance-none border border-zinc-200 dark:border-zinc-800 checked:relative checked:before:absolute checked:before:top-2.5 checked:before:left-1/2 checked:before:w-[0.8rem] checked:before:-translate-x-1/2 checked:before:-translate-y-1/2 checked:before:content-checkIcon checked:before:brightness-0 dark:checked:before:brightness-200"
+  "bg-white dark:bg-zinc-950 rounded-md h-5 w-5 cursor-pointer appearance-none border border-zinc-200 dark:border-zinc-800 checked:relative checked:before:absolute checked:before:top-2.5 checked:before:left-1/2 checked:before:w-[0.8rem] checked:before:-translate-x-1/2 checked:before:-translate-y-1/2 checked:before:content-checkIcon checked:before:brightness-0 dark:checked:before:brightness-200 focus:outline-none focus:ring-2 focus:ring-zinc-950 focus:ring-offset-2 dark:ring-offset-zinc-950 dark:focus:ring-zinc-300"
 );
--- src/components/Form/Select/styles.ts
+++ src/components/Form/Select/styles.ts
@@ -1,5 +1,5 @@
 import { cva } from "class-variance-authority";
 
 export const selectStyles = cva(
-  "w-full font-sans appearance-none rounded-lg border border-zinc-200 bg-white pl-3 pr-8 py-2 text-sm ring-offset-white placeholder:text-zinc-500 focus:outline-none focus:ring-2 focus:ring-zinc-950 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus:ring-zinc-300"
+  "w-full font-sans appearance-none rounded-lg border border-zinc-200 bg-white pl-3 pr-8 py-2 text-sm ring-offset-white placeholder:text-zinc-500 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 focus:outline-none focus:ring-2 focus:ring-zinc-950 focus:ring-offset-2 dark:ring-offset-zinc-950 dark:focus:ring-zinc-300 dark:placeholder:text-zinc-400"
 );

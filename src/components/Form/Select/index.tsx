464d371c1d65afa7ede77d0123f2da857119c86a
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-01-25 22:47:23 +0100

    apply twmerge to select

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -1,10 +1,15 @@
 import { forwardRef } from "react";
 import { SelectProps } from "./types";
 import { selectStyles } from "./styles";
+import { twMerge } from "tailwind-merge";
 
 export const Select = forwardRef<HTMLSelectElement, SelectProps>(
   ({ className, ...props }, ref) => (
-    <select className={selectStyles({ className })} ref={ref} {...props}>
+    <select
+      className={twMerge(selectStyles({ className }))}
+      ref={ref}
+      {...props}
+    >
       {props.children}
     </select>
   )

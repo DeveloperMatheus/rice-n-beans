18ecd10024f51476b2b8388544018739d1a216e1
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-02-17 00:28:42 +0100

    (base) wip

---
+++ src/app/base-test/desgraca/page.tsx
@@ -0,0 +1,3 @@
+export default function DesgracaPage() {
+  return <section>desgraaaca</section>;
+}
--- src/app/base-test/layout.tsx
+++ src/app/base-test/layout.tsx
@@ -1,11 +1,16 @@
 import { Base, BaseContainer, BaseDrawer, BaseHeader } from "~/components/Base";
 
-export default function Home() {
+export default function BaseTestLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <Base>
       <BaseDrawer>Test</BaseDrawer>
       <BaseContainer>
         <BaseHeader>Header</BaseHeader>
+        <div className="px-3 pb-3 pt-1">{children}</div>
       </BaseContainer>
     </Base>
   );

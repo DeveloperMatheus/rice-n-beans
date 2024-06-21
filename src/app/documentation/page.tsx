9f57863baf3e3ef6df980c791c43e20564b3acad
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-06-22 01:28:23 +0200

    refactor: Remove unused Link component from DocumentationPage

--- src/app/documentation/page.tsx
+++ src/app/documentation/page.tsx
@@ -1,11 +1,8 @@
-import Link from "next/link";
 import { Input } from "~/components/Form";
 
 export default function DocumentationPage() {
   return (
     <section>
-      <Link href="/base-test/base-nested">Go to nested base page</Link>
-
       <Input />
     </section>
   );

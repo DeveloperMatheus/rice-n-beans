97e577fedfe59bfcdba394231d9e6652f415cbed
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-06-30 00:21:24 +0200

    refactor: Add Documentation Examples page with options to navigate to different examples

---
+++ src/app/documentation/examples/page.tsx
@@ -0,0 +1,41 @@
+import { Braces, CodeXml } from "lucide-react";
+import Link from "next/link";
+import { Button, Card } from "~/components/Layout";
+import { Text } from "~/components/Typography";
+
+const listDocOptions: { icon: React.ReactNode; title: string; url: string }[] =
+  [
+    {
+      title: "Form",
+      icon: <CodeXml size={26} />,
+      url: "/documentation/examples/form",
+    },
+    {
+      title: "Layout",
+      icon: <Braces size={26} />,
+      url: "/documentation/examples/layout",
+    },
+  ];
+
+export default function DocumentationExamplesPage() {
+  return (
+    <section className="flex flex-col items-center justify-center h-full">
+      <div className="space-y-3 text-center">
+        <Text tag="h1">Examples</Text>
+
+        <Text tag="h2">Here you can check some examples:</Text>
+      </div>
+
+      <div className="flex items-center justify-center gap-3 mt-3 flex-wrap">
+        {listDocOptions.map((docOption, index) => (
+          <Link href={docOption.url} key={`${docOption.title}--${index}`}>
+            <Card className="flex flex-col items-center justify-center gap-2 min-w-40">
+              <div>{docOption.icon}</div>
+              <Text tag="h3">{docOption.title}</Text>
+            </Card>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+}

88187787406b298315ef5f5a407708875d12dc6f
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-06-30 00:46:09 +0200

    refactor: Update form page with improved layout and functionality

--- src/app/documentation/examples/form/page.tsx
+++ src/app/documentation/examples/form/page.tsx
@@ -1,5 +1,5 @@
-import { Input, Label } from "~/components/Form";
-import { Card } from "~/components/Layout";
+import { Checkbox, Input, Label } from "~/components/Form";
+import { Button, Card } from "~/components/Layout";
 import { Text } from "~/components/Typography";
 
 export default function DocumentationExampleFormPage() {
@@ -10,10 +10,39 @@ export default function DocumentationExampleFormPage() {
 
         <Card>
           <form>
-            <div className="space-y-3">
-              <div className="flex items-center justify-center gap-3">
-                <Label>Name</Label>
-                <Input placeholder="Type your name" />
+            <div className="grid grid-cols-12 gap-5">
+              <div className="col-span-12 xl:col-span-3">
+                <div className="flex items-center justify-center gap-2 w-full">
+                  <Label>Name</Label>
+                  <Input type="text" />
+                </div>
+              </div>
+              <div className="col-span-12 xl:col-span-3">
+                <div className="flex items-center justify-center gap-2 w-full">
+                  <Label>Surname</Label>
+                  <Input type="text" />
+                </div>
+              </div>
+              <div className="col-span-12 xl:col-span-3">
+                <div className="flex items-center justify-center gap-2 w-full">
+                  <Label>Email</Label>
+                  <Input type="email" />
+                </div>
+              </div>
+              <div className="col-span-12 xl:col-span-3">
+                <div className="flex items-center justify-center gap-2 w-full">
+                  <Label>Phone</Label>
+                  <Input type="tel" />
+                </div>
+              </div>
+              <div className="col-span-12">
+                <div className="flex items-center justify-center gap-5">
+                  <div className="flex items-center justify-center gap-2">
+                    <Label htmlFor="keep-updated">Keep me updated</Label>
+                    <Checkbox id="keep-updated" />
+                  </div>
+                  <Button>Submit</Button>
+                </div>
               </div>
             </div>
           </form>

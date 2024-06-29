5ecc47600af637e18f3b2d462169bfaf48a008a8
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-06-30 01:09:00 +0200

    refactor: Update form page with improved layout and functionality

--- src/app/documentation/examples/form/page.tsx
+++ src/app/documentation/examples/form/page.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, Input, Label } from "~/components/Form";
+import { Checkbox, Input, Label, Select, Switch } from "~/components/Form";
 import { Button, Card } from "~/components/Layout";
 import { Text } from "~/components/Typography";
 
@@ -8,42 +8,18 @@ export default function DocumentationExampleFormPage() {
       <div className="space-y-3">
         <Text tag="h1">Example Form</Text>
 
-        <Card>
-          <form>
-            <div className="grid grid-cols-12 gap-5">
-              <div className="col-span-12 xl:col-span-3">
-                <div className="flex items-center justify-center gap-2 w-full">
-                  <Label>Name</Label>
-                  <Input type="text" />
-                </div>
-              </div>
-              <div className="col-span-12 xl:col-span-3">
-                <div className="flex items-center justify-center gap-2 w-full">
-                  <Label>Surname</Label>
-                  <Input type="text" />
-                </div>
-              </div>
-              <div className="col-span-12 xl:col-span-3">
-                <div className="flex items-center justify-center gap-2 w-full">
-                  <Label>Email</Label>
-                  <Input type="email" />
-                </div>
-              </div>
-              <div className="col-span-12 xl:col-span-3">
-                <div className="flex items-center justify-center gap-2 w-full">
-                  <Label>Phone</Label>
-                  <Input type="tel" />
-                </div>
-              </div>
-              <div className="col-span-12">
-                <div className="flex items-center justify-center gap-5">
-                  <div className="flex items-center justify-center gap-2">
-                    <Label htmlFor="keep-updated">Keep me updated</Label>
-                    <Checkbox id="keep-updated" />
-                  </div>
-                  <Button>Submit</Button>
-                </div>
-              </div>
+        <Card className="w-fit">
+          <form className="space-y-3">
+            <div className="space-y-1">
+              <Label>Login</Label>
+              <Input type="text" />
+            </div>
+            <div className="space-y-1">
+              <Label>Password</Label>
+              <Input type="password" />
+            </div>
+            <div className="text-center">
+              <Button className="w-32">Submit</Button>
             </div>
           </form>
         </Card>

ad3211c237c01281f062b0077a5ad04c252f0a30
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-07-03 22:42:36 +0200

    feat: Add InstallationStep1 component for package manager selection

---
+++ src/app/documentation/installation/components/InstallationStep1.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState } from "react";
+import { Button, Card } from "~/components/Layout";
+
+type Command = "npm" | "yarn" | "pnpm";
+
+export const InstallationStep1 = () => {
+  const [commandSelected, setCommandSelected] = useState<Command>("pnpm");
+
+  function renderCommandPrefix() {
+    const commands = {
+      npm: "$ npm install",
+      yarn: "$ yarn add",
+      pnpm: "$ pnpm install",
+    };
+
+    return commands[commandSelected];
+  }
+
+  function handleSelectedCommand(buttonCommand: Command) {
+    return commandSelected === buttonCommand ? "default" : "outline";
+  }
+
+  return (
+    <div>
+      <div className="flex items-center justify-center xl:justify-start gap-3 my-3">
+        <Button
+          variant={handleSelectedCommand("yarn")}
+          onClick={() => setCommandSelected("yarn")}
+        >
+          Yarn
+        </Button>
+        <Button
+          variant={handleSelectedCommand("npm")}
+          onClick={() => setCommandSelected("npm")}
+        >
+          NPM
+        </Button>
+        <Button
+          variant={handleSelectedCommand("pnpm")}
+          onClick={() => setCommandSelected("pnpm")}
+        >
+          PNPM
+        </Button>
+      </div>
+
+      <Card>
+        <div>
+          <code>{renderCommandPrefix()} typescript</code>
+        </div>
+        <div>
+          <code>{renderCommandPrefix()} tailwindcss postcss autoprefixer</code>
+        </div>
+        <div>
+          <code>{renderCommandPrefix()} class-variance-authority</code>
+        </div>
+      </Card>
+    </div>
+  );
+};

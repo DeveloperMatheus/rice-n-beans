cd359b1ba17fb770cb3dd4cb80e4402528427709
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-07-19 23:30:23 +0200

    feat: Update Form component to use class-variance-authority and replace hasError prop with isInvalid prop

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -6,7 +6,7 @@ const inputStyles = cva(
   "appearance-none font-sans w-full rounded-lg border border-zinc-200 bg-white px-3 py-2 ring-offset-white file:border-0 file:bg-transparent file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
   {
     variants: {
-      hasError: {
+      isInvalid: {
         true: "border-red-500 dark:border-red-900 focus-visible:ring-red-500 dark:focus-visible:ring-red-900",
       },
     },
@@ -32,9 +32,9 @@ const errorMessageStyles = cva("text-red-500 font-sans");
 export const Input = forwardRef<
   HTMLInputElement,
   ComponentProps<"input"> & VariantProps<typeof inputStyles>
->(({ className, hasError, ...props }, ref) => (
+>(({ className, isInvalid, ...props }, ref) => (
   <input
-    className={twMerge(inputStyles({ className, hasError }))}
+    className={twMerge(inputStyles({ className, isInvalid }))}
     ref={ref}
     {...props}
   />

f158872ca779cf07d3f5ab36545018c45179ff58
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-07-19 22:37:14 +0200

    refactor: Update Form component to use class-variance-authority and place hasError prop with it's styles

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,9 +1,17 @@
-import { cva } from "class-variance-authority";
+import { cva, VariantProps } from "class-variance-authority";
 import { ComponentProps, forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-const inputStyles =
-  "appearance-none font-sans w-full rounded-lg border border-zinc-200 bg-white px-3 py-2 ring-offset-white file:border-0 file:bg-transparent file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300";
+const inputStyles = cva(
+  "appearance-none font-sans w-full rounded-lg border border-zinc-200 bg-white px-3 py-2 ring-offset-white file:border-0 file:bg-transparent file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
+  {
+    variants: {
+      hasError: {
+        true: "border-red-500 dark:border-red-900 focus-visible:ring-red-500 dark:focus-visible:ring-red-900",
+      },
+    },
+  }
+);
 const labelStyles =
   "inline-block font-sans text-black dark:text-zinc-200 font-bold select-none";
 const checkboxStyles =
@@ -21,11 +29,16 @@ const datePickerStyles =
 const errorMessageStyles = cva("text-red-500 font-sans");
 
 /* --- Input --- */
-export const Input = forwardRef<HTMLInputElement, ComponentProps<"input">>(
-  ({ className, ...props }, ref) => (
-    <input className={twMerge(inputStyles, className)} ref={ref} {...props} />
-  )
-);
+export const Input = forwardRef<
+  HTMLInputElement,
+  ComponentProps<"input"> & VariantProps<typeof inputStyles>
+>(({ className, hasError, ...props }, ref) => (
+  <input
+    className={twMerge(inputStyles({ className, hasError }))}
+    ref={ref}
+    {...props}
+  />
+));
 
 Input.displayName = "Input";
 

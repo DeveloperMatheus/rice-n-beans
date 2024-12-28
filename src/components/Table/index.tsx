693bd3f6c9a4d0213cda0f380ab3cb3d31e60799
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2024-12-28 12:50:00 +0100

    refactor[reactv19]: update table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,92 +1,97 @@
-import { ComponentProps, forwardRef } from "react";
-import { twMerge } from "tailwind-merge";
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-const tableStyles = "border-collapse text-black dark:text-zinc-200";
-const tableDataStyles = "px-2 py-1 text-center";
+const tableStyles = 'border-collapse text-black dark:text-zinc-200'
+const tableDataStyles = 'px-2 py-1 text-center'
 const tableRowStyles =
-  "[&:not(:last-child)]:border-b border-zinc-200 dark:border-zinc-800";
-const tableHeadStyles = "px-2 py-1 text-center";
+  '[&:not(:last-child)]:border-b border-zinc-200 dark:border-zinc-800'
+const tableHeadStyles = 'px-2 py-1 text-center'
 const tableHeaderStyles =
-  "border-b border-zinc-200 dark:border-zinc-800 text-center";
+  'border-b border-zinc-200 dark:border-zinc-800 text-center'
 const tableFooterStyles =
-  "border-t border-zinc-200 dark:border-zinc-800 font-bold";
+  'border-t border-zinc-200 dark:border-zinc-800 font-bold'
 
 /* --- Table --- */
-export const Table = forwardRef<HTMLTableElement, ComponentProps<"table">>(
-  ({ children, className, ...props }, ref) => (
-    <table className={twMerge(tableStyles, className)} ref={ref} {...props}>
-      {children}
-    </table>
-  )
-);
+const Table = ({ children, className, ...props }: ComponentProps<'table'>) => (
+  <table className={twMerge(tableStyles, className)} {...props}>
+    {children}
+  </table>
+)
 
-Table.displayName = "Table";
+Table.displayName = 'Table'
 
 /* --- TableBody --- */
-export const TableBody = forwardRef<
-  HTMLTableSectionElement,
-  ComponentProps<"tbody">
->(({ children, className, ...props }, ref) => (
-  <tbody className={twMerge(className)} ref={ref} {...props}>
+const TableBody = ({
+  children,
+  className,
+  ...props
+}: ComponentProps<'tbody'>) => (
+  <tbody className={twMerge(className)} {...props}>
     {children}
   </tbody>
-));
+)
 
-TableBody.displayName = "TableBody";
+TableBody.displayName = 'TableBody'
 
 /* --- TableData --- */
-export const TableData = forwardRef<HTMLTableCellElement, ComponentProps<"td">>(
-  ({ children, className, ...props }, ref) => (
-    <td className={twMerge(tableDataStyles, className)} ref={ref} {...props}>
-      {children}
-    </td>
-  )
-);
+const TableData = ({ children, className, ...props }: ComponentProps<'td'>) => (
+  <td className={twMerge(tableDataStyles, className)} {...props}>
+    {children}
+  </td>
+)
 
-TableData.displayName = "TableData";
+TableData.displayName = 'TableData'
 
 /* --- TableRow --- */
-export const TableRow = forwardRef<HTMLTableRowElement, ComponentProps<"tr">>(
-  ({ children, className, ...props }, ref) => (
-    <tr className={twMerge(tableRowStyles, className)} ref={ref} {...props}>
-      {children}
-    </tr>
-  )
-);
+const TableRow = ({ children, className, ...props }: ComponentProps<'tr'>) => (
+  <tr className={twMerge(tableRowStyles, className)} {...props}>
+    {children}
+  </tr>
+)
 
-TableRow.displayName = "TableRow";
+TableRow.displayName = 'TableRow'
 
 /* --- TableHead --- */
-export const TableHead = forwardRef<HTMLTableCellElement, ComponentProps<"th">>(
-  ({ children, className, ...props }, ref) => (
-    <th className={twMerge(tableHeadStyles, className)} ref={ref} {...props}>
-      {children}
-    </th>
-  )
-);
+const TableHead = ({ children, className, ...props }: ComponentProps<'th'>) => (
+  <th className={twMerge(tableHeadStyles, className)} {...props}>
+    {children}
+  </th>
+)
 
-TableHead.displayName = "TableHead";
+TableHead.displayName = 'TableHead'
 
 /* --- TableHeader --- */
-export const TableHeader = forwardRef<
-  HTMLTableSectionElement,
-  ComponentProps<"thead">
->(({ children, className, ...props }, ref) => (
-  <thead className={twMerge(tableHeaderStyles, className)} ref={ref} {...props}>
+const TableHeader = ({
+  children,
+  className,
+  ...props
+}: ComponentProps<'thead'>) => (
+  <thead className={twMerge(tableHeaderStyles, className)} {...props}>
     {children}
   </thead>
-));
+)
 
-TableHeader.displayName = "TableHeader";
+TableHeader.displayName = 'TableHeader'
 
 /* --- TableFooter --- */
-export const TableFooter = forwardRef<
-  HTMLTableSectionElement,
-  ComponentProps<"tfoot">
->(({ children, className, ...props }, ref) => (
-  <tfoot className={twMerge(tableFooterStyles, className)} ref={ref} {...props}>
+const TableFooter = ({
+  children,
+  className,
+  ...props
+}: ComponentProps<'tfoot'>) => (
+  <tfoot className={twMerge(tableFooterStyles, className)} {...props}>
     {children}
   </tfoot>
-));
+)
 
-TableFooter.displayName = "TableFooter";
+TableFooter.displayName = 'TableFooter'
+
+export {
+  Table,
+  TableBody,
+  TableData,
+  TableRow,
+  TableHead,
+  TableHeader,
+  TableFooter
+}

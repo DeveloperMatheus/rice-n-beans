cab1faef8b939163a8b34e9cfe21cb3dd01bf739
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2025-02-08 01:07:39 +0100

    fix[project]: use env variables for base url

--- src/services/code/index.ts
+++ src/services/code/index.ts
@@ -1,12 +1,13 @@
 import { CodeResponse } from '~/app/api/get-code/route'
 
 async function getCode(componentName: string): Promise<CodeResponse> {
+  const baseUrl = process.env.API_BASE_URL
+
   const response = await fetch(
-    `http://localhost:3000/api/get-code?component=${componentName}`,
+    `${baseUrl}/api/get-code?component=${componentName}`,
     {
       method: 'GET',
       headers: {
-        component: 'Accordion',
         'Content-Type': 'application/json'
       }
     }

6cc446e679e1d31b21138213834ac1fe0a25a226
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2025-02-17 22:58:39 +0100

    feat[project]: remove unused react import from unit tests

--- src/components/Dialog/__tests__/Dialog.test.tsx
+++ src/components/Dialog/__tests__/Dialog.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { render, screen, fireEvent } from '@testing-library/react'
 import { DialogProvider, useConfirm, DialogModal } from '..'
 import { Button } from '~/components/Button'

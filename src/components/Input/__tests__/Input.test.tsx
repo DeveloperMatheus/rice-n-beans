e4fa998104031fb08c4cec0032748e2dd6f989a8
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2025-02-19 22:03:15 +0100

    feat[input]: make use of css vars for text, update tests and docs

--- src/components/Input/__tests__/Input.test.tsx
+++ src/components/Input/__tests__/Input.test.tsx
@@ -7,42 +7,43 @@ describe('Input Component', () => {
 
     const input = screen.getByRole('textbox')
     expect(input).toBeInTheDocument()
-    expect(input).toHaveClass('rounded-lg') // Default border radius
-    expect(input).toHaveClass('border') // Default border
-    expect(input).toHaveClass('border-default') // Default border color
-    expect(input).toHaveClass('bg-scaffold') // Default background color
-    expect(input).toHaveClass('px-3') // Default padding
-    expect(input).toHaveClass('py-2') // Default padding
+    expect(input).toHaveClass('text-contrast')
+    expect(input).toHaveClass('rounded-lg')
+    expect(input).toHaveClass('border')
+    expect(input).toHaveClass('border-default')
+    expect(input).toHaveClass('bg-scaffold')
+    expect(input).toHaveClass('px-3')
+    expect(input).toHaveClass('py-2')
   })
 
   it('should render the Input component as an input element', () => {
     render(<Input />)
 
     const input = screen.getByRole('textbox')
-    expect(input.tagName).toBe('INPUT') // Ensure it's an input element
+    expect(input.tagName).toBe('INPUT')
   })
 
   it('should apply the correct styles when isInvalid is true', () => {
     render(<Input isInvalid />)
 
     const input = screen.getByRole('textbox')
-    expect(input).toHaveClass('focus-visible:ring-error') // Invalid state class
+    expect(input).toHaveClass('focus-visible:ring-error')
   })
 
   it('should merge custom class names with the base styles', () => {
     render(<Input className="custom-class" />)
 
     const input = screen.getByRole('textbox')
-    expect(input).toHaveClass('custom-class') // Custom class
-    expect(input).toHaveClass('rounded-lg') // Default border radius
+    expect(input).toHaveClass('custom-class')
+    expect(input).toHaveClass('rounded-lg')
   })
 
   it('should apply the correct styles when disabled', () => {
     render(<Input disabled />)
 
     const input = screen.getByRole('textbox')
-    expect(input).toBeDisabled() // Check if the input is disabled
-    expect(input).toHaveClass('disabled:cursor-not-allowed') // Disabled state class
-    expect(input).toHaveClass('disabled:opacity-50') // Disabled state class
+    expect(input).toBeDisabled()
+    expect(input).toHaveClass('disabled:cursor-not-allowed')
+    expect(input).toHaveClass('disabled:opacity-50')
   })
 })

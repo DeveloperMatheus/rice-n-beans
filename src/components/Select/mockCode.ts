9ad6a044e434529dbada2b572cce67f7f8016298
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2025-02-20 23:32:06 +0100

    feat[select]: update code documentation

--- src/components/Select/mockCode.ts
+++ src/components/Select/mockCode.ts
@@ -5,14 +5,14 @@ import { extendTailwindMerge } from 'tailwind-merge'
 const twMerge = extendTailwindMerge({
   extend: {
     classGroups: {
-      'bg-image': ['bg-select-icon'],
+      'bg-image': ['bg-arrow-icon'],
       'bg-color': ['bg-scaffold']
     }
   }
 })
 
 const selectStyles = cva(
-  'text-contrast border-default placeholder:placeholder-muted focus-visible:ring-default bg-scaffold bg-select-icon w-full appearance-none rounded-lg border bg-[length:1.6rem] bg-[position:center_right_0.75rem] bg-no-repeat py-2 pr-8 pl-3 focus-visible:ring-2 focus-visible:outline-hidden focus-visible:outline-offset-0 disabled:cursor-not-allowed disabled:opacity-50',
+  'text-contrast border-default placeholder:placeholder-muted focus-visible:ring-default bg-scaffold bg-arrow-icon w-full appearance-none rounded-lg border bg-[length:1.6rem] bg-[position:center_right_0.75rem] bg-no-repeat py-2 pr-8 pl-3 focus-visible:ring-2 focus-visible:outline-hidden focus-visible:outline-offset-0 disabled:cursor-not-allowed disabled:opacity-50',
   {
     variants: {
       isInvalid: {

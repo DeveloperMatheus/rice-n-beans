e730138a79d0685781f098f8c5992b4f912a9168
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2025-11-19 23:40:07 +0100

    [base]: refactor code to tailwind-variants and update unit tests

--- src/components/Base/index.tsx
+++ src/components/Base/index.tsx
@@ -1,37 +1,41 @@
 import { ComponentProps } from 'react'
 
-import { VariantProps, cva } from 'class-variance-authority'
-import { twMerge } from 'tailwind-merge'
-
 import { Button } from '~/components/Button'
+import { tv, VariantProps } from 'tailwind-variants'
 
-const baseStyles = cva('flex', {
+const baseStyles = tv({
+  slots: {
+    wrapper: 'flex',
+    container: 'h-screen w-full overflow-hidden',
+    content:
+      'bg-scaffold border-default h-[calc(100vh-4.1875rem)] overflow-auto border-t p-3 lg:border-y',
+    drawer:
+      'bg-scaffold border-default fixed top-0 right-0 left-0 z-10 h-screen overflow-x-hidden overflow-y-auto border p-3 transition-all lg:sticky lg:overflow-x-hidden lg:overflow-y-auto',
+    header:
+      'bg-scaffold border-default sticky top-0 w-full p-3 shadow-md lg:border-t',
+    toggle: 'text-2xl'
+  },
   variants: {
     orientation: {
-      left: 'flex-row',
-      right: 'flex-row-reverse'
-    }
-  }
-})
-const baseContainerStyles = 'h-screen w-full overflow-hidden'
-const baseContentStyles =
-  'bg-scaffold border-default h-[calc(100vh-4.1875rem)] overflow-auto border-t p-3 lg:border-y'
-
-const baseDrawerStyles = cva(
-  'bg-scaffold border-default fixed top-0 right-0 left-0 z-10 h-screen overflow-x-hidden overflow-y-auto border p-3 transition-all lg:sticky lg:overflow-x-hidden lg:overflow-y-auto',
-  {
-    variants: {
-      isOpen: {
-        true: 'w-full lg:w-72 lg:p-5',
-        false: 'hidden lg:block lg:w-14'
+      left: {
+        wrapper: 'flex-row'
+      },
+      right: {
+        wrapper: 'flex-row-reverse'
+      }
+    },
+    isOpen: {
+      true: {
+        drawer: 'w-full lg:w-72 lg:p-5'
+      },
+      false: {
+        drawer: 'hidden lg:block lg:w-14'
       }
     }
   }
-)
-const baseHeaderStyles =
-  'bg-scaffold border-default sticky top-0 w-full p-3 shadow-md lg:border-t'
+})
 
-const baseToggleDrawerStyles = 'text-2xl'
+const { wrapper, container, content, drawer, header, toggle } = baseStyles()
 
 /* --- Base --- */
 const Base = ({
@@ -39,9 +43,9 @@ const Base = ({
   className,
   orientation = 'left',
   ...props
-}: ComponentProps<'div'> & VariantProps<typeof baseStyles>) => {
+}: ComponentProps<'div'> & Omit<VariantProps<typeof baseStyles>, 'isOpen'>) => {
   return (
-    <div className={twMerge(baseStyles({ className, orientation }))} {...props}>
+    <div className={wrapper({ className, orientation })} {...props}>
       {children}
     </div>
   )
@@ -55,7 +59,7 @@ const BaseContainer = ({
   className,
   ...props
 }: ComponentProps<'div'>) => (
-  <div className={twMerge(baseContainerStyles, className)} {...props}>
+  <div className={container({ className })} {...props}>
     {children}
   </div>
 )
@@ -68,7 +72,7 @@ const BaseContent = ({
   className,
   ...props
 }: ComponentProps<'main'>) => (
-  <main className={twMerge(baseContentStyles, className)} {...props}>
+  <main className={content({ className })} {...props}>
     {children}
   </main>
 )
@@ -81,12 +85,10 @@ const BaseDrawer = ({
   className,
   isOpen,
   ...props
-}: ComponentProps<'nav'> & VariantProps<typeof baseDrawerStyles>) => {
+}: ComponentProps<'nav'> &
+  Omit<VariantProps<typeof baseStyles>, 'orientation'>) => {
   return (
-    <nav
-      className={twMerge(baseDrawerStyles({ className, isOpen }))}
-      {...props}
-    >
+    <nav className={drawer({ className, isOpen })} {...props}>
       {children}
     </nav>
   )
@@ -101,7 +103,7 @@ const BaseHeader = ({
   ...props
 }: ComponentProps<'div'>) => {
   return (
-    <div className={twMerge(baseHeaderStyles, className)} {...props}>
+    <div className={header({ className })} {...props}>
       {children}
     </div>
   )
@@ -117,7 +119,7 @@ const BaseToggleDrawer = ({
   return (
     <Button
       variant="ghost"
-      className={twMerge(baseToggleDrawerStyles, className)}
+      className={toggle({ className })}
       size="icon"
       {...props}
     >

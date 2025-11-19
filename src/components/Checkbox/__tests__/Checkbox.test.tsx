500a208bf358f400b71c8f8f6ba05c3eef8630ca
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2025-11-19 23:49:23 +0100

    [checkbox]: refactor code to tailwind-variants and update unit tests

--- src/components/Checkbox/__tests__/Checkbox.test.tsx
+++ src/components/Checkbox/__tests__/Checkbox.test.tsx
@@ -7,40 +7,37 @@ describe('Checkbox Component', () => {
 
     const checkbox = screen.getByRole('checkbox')
     expect(checkbox).toBeInTheDocument()
-    expect(checkbox).toHaveClass('bg-scaffold') // Default background color
-    expect(checkbox).toHaveClass('rounded-md') // Default border radius
-    expect(checkbox).toHaveClass('min-h-5') // Default minimum height
-    expect(checkbox).toHaveClass('min-w-5') // Default minimum width
-    expect(checkbox).toHaveClass('cursor-pointer') // Default cursor
-    expect(checkbox).toHaveClass('border') // Default border
-    expect(checkbox).toHaveClass('border-default') // Default border color
+    expect(checkbox).toHaveClass('bg-scaffold')
+    expect(checkbox).toHaveClass('rounded-md')
+    expect(checkbox).toHaveClass('min-h-5')
+    expect(checkbox).toHaveClass('min-w-5')
+    expect(checkbox).toHaveClass('cursor-pointer')
+    expect(checkbox).toHaveClass('border')
+    expect(checkbox).toHaveClass('border-default')
   })
 
   it('should render the Checkbox component as an input of type checkbox', () => {
     render(<Checkbox />)
 
     const checkbox = screen.getByRole('checkbox')
-    expect(checkbox).toHaveAttribute('type', 'checkbox') // Ensure it's a checkbox input
+    expect(checkbox).toHaveAttribute('type', 'checkbox')
   })
 
   it('should apply the correct styles when checked', () => {
     render(<Checkbox defaultChecked />)
 
     const checkbox = screen.getByRole('checkbox')
-    expect(checkbox).toBeChecked() // Ensure the checkbox is checked
+    expect(checkbox).toBeChecked()
 
-    // Check for the pseudo-element styles when checked
     expect(checkbox).toHaveClass('checked:before:absolute')
     expect(checkbox).toHaveClass('checked:before:top-1/2')
     expect(checkbox).toHaveClass('checked:before:left-1/2')
-    expect(checkbox).toHaveClass('checked:before:w-[1rem]')
-    expect(checkbox).toHaveClass('checked:before:h-[1rem]')
+    expect(checkbox).toHaveClass('checked:before:w-4')
+    expect(checkbox).toHaveClass('checked:before:h-4')
     expect(checkbox).toHaveClass('checked:before:leading-none')
     expect(checkbox).toHaveClass('checked:before:-translate-x-1/2')
     expect(checkbox).toHaveClass('checked:before:-translate-y-1/2')
-    expect(checkbox).toHaveClass(
-      'checked:before:content-[var(--checkmark-icon)]'
-    )
+    expect(checkbox).toHaveClass('checked:before:content-(--checkmark-icon)')
     expect(checkbox).toHaveClass('checked:before:brightness-0')
     expect(checkbox).toHaveClass('dark:checked:before:brightness-200')
   })
@@ -49,16 +46,16 @@ describe('Checkbox Component', () => {
     render(<Checkbox disabled />)
 
     const checkbox = screen.getByRole('checkbox')
-    expect(checkbox).toBeDisabled() // Ensure the checkbox is disabled
-    expect(checkbox).toHaveClass('disabled:cursor-not-allowed') // Disabled state class
-    expect(checkbox).toHaveClass('disabled:opacity-50') // Disabled state class
+    expect(checkbox).toBeDisabled()
+    expect(checkbox).toHaveClass('disabled:cursor-not-allowed')
+    expect(checkbox).toHaveClass('disabled:opacity-50')
   })
 
   it('should merge custom class names with the base styles', () => {
     render(<Checkbox className="custom-class" />)
 
     const checkbox = screen.getByRole('checkbox')
-    expect(checkbox).toHaveClass('custom-class') // Custom class
-    expect(checkbox).toHaveClass('bg-scaffold') // Default background color
+    expect(checkbox).toHaveClass('custom-class')
+    expect(checkbox).toHaveClass('bg-scaffold')
   })
 })
--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,15 +1,12 @@
 import { ComponentProps } from 'react'
-import { twMerge } from 'tailwind-merge'
+import { tv } from 'tailwind-variants'
 
-const checkboxStyles =
-  'bg-scaffold border-default relative min-h-5 min-w-5 cursor-pointer appearance-none rounded-md border checked:before:absolute checked:before:top-1/2 checked:before:left-1/2 checked:before:h-[1rem] checked:before:w-[1rem] checked:before:-translate-x-1/2 checked:before:-translate-y-1/2 checked:before:leading-none checked:before:brightness-0 checked:before:content-[var(--checkmark-icon)] disabled:cursor-not-allowed disabled:opacity-50 dark:checked:before:brightness-200'
+const checkboxStyles = tv({
+  base: 'bg-scaffold border-default relative min-h-5 min-w-5 cursor-pointer appearance-none rounded-md border checked:before:absolute checked:before:top-1/2 checked:before:left-1/2 checked:before:h-4 checked:before:w-4 checked:before:-translate-x-1/2 checked:before:-translate-y-1/2 checked:before:leading-none checked:before:brightness-0 checked:before:content-(--checkmark-icon) disabled:cursor-not-allowed disabled:opacity-50 dark:checked:before:brightness-200'
+})
 
 const Checkbox = ({ className, ...props }: ComponentProps<'input'>) => (
-  <input
-    type="checkbox"
-    className={twMerge(checkboxStyles, className)}
-    {...props}
-  />
+  <input type="checkbox" className={checkboxStyles({ className })} {...props} />
 )
 
 Checkbox.displayName = 'Checkbox'

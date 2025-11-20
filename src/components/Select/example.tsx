af239b1765ece9cd0910cd4d068a238b87667d46
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2025-11-21 00:36:52 +0100

    [select]: make docs automatic

---
+++ src/components/Select/example.tsx
@@ -0,0 +1,16 @@
+import { Select } from '.'
+
+const Example = () => {
+  return (
+    <Select>
+      <option defaultChecked hidden>
+        Select an option
+      </option>
+      <option value="1">Option 1</option>
+      <option value="2">Option 2</option>
+      <option value="3">Option 3</option>
+    </Select>
+  )
+}
+
+export { Example }

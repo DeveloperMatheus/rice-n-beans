0f25d929924c917d0833da7f9568cf9d03aabcea
Matheus de Sousa Menezes <matheus.developer@gmail.com>
2025-11-22 20:02:19 +0100

    [project]: apply layout for components route

---
+++ src/app/components/[component]/layout.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import {
+  Base,
+  BaseToggleDrawer,
+  BaseContainer,
+  BaseDrawer,
+  BaseHeader,
+  BaseContent
+} from '~/components/base'
+import { ThemeSwitch } from '~/components/Theme/ThemeSwitch'
+import { useCallback, useEffect, useState } from 'react'
+import { DrawerItems } from '~/app/documentation/components/DrawerItems'
+
+export default function ComponentsLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  const [isClient, setIsClient] = useState(false)
+  const [isOpen, setOpen] = useState(false)
+
+  const isMobile = useCallback(() => /Mobi/i.test(navigator.userAgent), [])
+
+  function toggleDrawer() {
+    setOpen(!isOpen)
+  }
+
+  function handleToggleDrawer() {
+    if (!isMobile()) return
+    setOpen(false)
+  }
+
+  useEffect(() => {
+    setIsClient(true)
+
+    if (isMobile()) return
+    setOpen(true)
+  }, [isMobile])
+
+  if (!isClient) return
+
+  return (
+    <Base>
+      <BaseDrawer isOpen={isOpen}>
+        <div className="flex flex-row items-center justify-between lg:hidden">
+          <BaseToggleDrawer
+            aria-label="Toggle drawer"
+            onClick={() => toggleDrawer()}
+          />
+          <ThemeSwitch />
+        </div>
+        <DrawerItems
+          isOpen={isOpen}
+          onClickDrawerItem={() => handleToggleDrawer()}
+        />
+      </BaseDrawer>
+      <BaseContainer>
+        <BaseHeader className="flex items-center justify-between gap-3">
+          <BaseToggleDrawer
+            aria-label="Toggle drawer"
+            onClick={() => toggleDrawer()}
+          />
+          <ThemeSwitch />
+        </BaseHeader>
+        <BaseContent>{children}</BaseContent>
+      </BaseContainer>
+    </Base>
+  )
+}
